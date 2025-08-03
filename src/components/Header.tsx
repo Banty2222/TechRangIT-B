6d146555f01420649c7652f7603ec3799fc9f0d7
bharath.koti1 <bharath.koti1@gmail.com>
2025-08-03 07:22:34 +0000

    on header section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -32,7 +32,7 @@ const Header = () => {
 
   return (
     <header className={`fixed top-0 left-0 right-0 z-50 header-blur transition-all duration-300 ${
-      isScrolled ? 'scrolled' : 'bg-transparent'
+      isScrolled ? 'scrolled' : ''
     }`}>
       <div className="container mx-auto px-4 lg:px-8">
         <div className="flex items-center justify-between h-16">
